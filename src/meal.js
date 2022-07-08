5e56b18798fb32fab44f896aa6fc40d39c11182e
Beth Wilson <bethwilson777@gmail.com>
2022-07-07 20:47:23 -0600

    More homework

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,34 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`
 }
 
+function createMenuItem() {
+  var menuItem = {
+    name: "Delicious French Toast",
+    price: "10.99",
+    type: "breakfast"
+  }
+  return menuItem
+}
+
+function addIngredients(food, ingredients) {
+  if (ingredients.includes(food)) {
+    ingredients.pop();
+  }
+    ingredients[ingredients.length] = food;
+    console.log(ingredients);
+    //return ingredients
+}
+
+function formatPrice() {
+  return `$`+ price; 
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
