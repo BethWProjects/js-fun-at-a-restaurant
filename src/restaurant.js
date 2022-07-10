3fbf2dd5a3e463110b15b49d2e44d5ceb03bc1b4
Beth Wilson <bethwilson777@gmail.com>
2022-07-09 20:33:22 -0600

    CreateRestaurant last test pass

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,15 +1,14 @@
 function createRestaurant(name, menus, pizzaRestaurant, arepaRestaurant, breakfast, lunch, dinner) {
-
   pizzaRestaurant =
   {
     name: name,
-    menus: {breakfast, lunch, dinner}
+    menus: {breakfast:[], lunch:[], dinner:[]}
   }
 
   //var pizzaRestaurant;
   console.log(pizzaRestaurant)
   console.log(arepaRestaurant)
-  console.log(pizzaRestaurant.menus)
+  console.log("Menus test: ", pizzaRestaurant.menus)
   //console.log(pizzaRestaurant.menus)
   // for (var i = 0; i < pizzaRestaurant; i++)
   // if (pizzaRestaurant.name[i])
