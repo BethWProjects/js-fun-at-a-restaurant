489d505f7d97866a33b8480321be26e5d6b4360a
Beth Wilson <bethwilson777@gmail.com>
2022-07-09 20:19:17 -0600

    Answers for solo project in order and restaurant file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order)
+  }
+  return deliveryOrders
+}
+
+function refundOrder(num, deliveryOrders) {
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === num ) {
+      deliveryOrders.splice([i], 1);
+      return deliveryOrders
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  let items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item);
+  }
+  return items.join(", ");
+}
+
+function searchOrder(deliveryOrders) {
+  for (var i = 0; deliveryOrders.length; i++) {
+    if (deliveryOrders.includes[i])
+    return deliveryOrders
+}
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,23 @@
+function createRestaurant(name, menus, pizzaRestaurant, arepaRestaurant, breakfast, lunch, dinner) {
 
+  pizzaRestaurant =
+  {
+    name: name,
+    menus: {breakfast, lunch, dinner}
+  }
+
+  //var pizzaRestaurant;
+  console.log(pizzaRestaurant)
+  console.log(arepaRestaurant)
+  console.log(pizzaRestaurant.menus)
+  //console.log(pizzaRestaurant.menus)
+  // for (var i = 0; i < pizzaRestaurant; i++)
+  // if (pizzaRestaurant.name[i])
+  return pizzaRestaurant
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant,
   // addMenuItem,
   // removeMenuItem
 }
